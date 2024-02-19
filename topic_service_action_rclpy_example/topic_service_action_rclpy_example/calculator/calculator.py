89bd808e76619f202d53f4dc28c092eb8acb1e2d
Yeongmin Kim <88310751+yeongmin01@users.noreply.github.com>
2024-02-19 17:45:45 +0900

    topic_service_action node
    subscriber, service server, action server

---
+++ topic_service_action_rclpy_example/topic_service_action_rclpy_example/calculator/calculator.py
@@ -0,0 +1,138 @@
+import time
+import rclpy
+
+from rclpy.executors import MultiThreadedExecutor
+
+from msg_srv_action_interface_example.action import ArithmeticChecker
+from msg_srv_action_interface_example.msg import ArithmeticArgument
+from msg_srv_action_interface_example.srv import ArithmeticOperator
+from rclpy.action import ActionServer
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.node import Node
+from rclpy.qos import QoSDurabilityPolicy
+from rclpy.qos import QoSHistoryPolicy
+from rclpy.qos import QoSProfile
+from rclpy.qos import QoSReliabilityPolicy
+
+
+class Calculator(Node):
+
+    def __init__(self):
+        super().__init__('calculator')
+        self.argument_a = 0.0
+        self.argument_b = 0.0
+        self.argument_operator = 0
+        self.argument_result = 0.0
+        self.argument_formula = ''
+        self.operator = ['+', '-', '*', '/']
+        self.callback_group = ReentrantCallbackGroup()
+
+        self.declare_parameter('qos_depth', 10)
+        qos_depth = self.get_parameter('qos_depth').value
+
+        QOS_RKL10V = QoSProfile(
+            reliability=QoSReliabilityPolicy.RELIABLE,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=qos_depth,
+            durability=QoSDurabilityPolicy.VOLATILE)
+
+        self.arithmetic_argument_subscriber = self.create_subscription(
+            ArithmeticArgument,
+            'arithmetic_argument',
+            self.get_arithmetic_argument,
+            QOS_RKL10V,
+            callback_group=self.callback_group)
+
+        self.arithmetic_service_server = self.create_service(
+            ArithmeticOperator,
+            'arithmetic_operator',
+            self.get_arithmetic_operator,
+            callback_group=self.callback_group)
+
+        self.arithmetic_action_server = ActionServer(
+            self,
+            ArithmeticChecker,
+            'arithmetic_checker',
+            self.execute_checker,
+            callback_group=self.callback_group)
+
+    def get_arithmetic_argument(self, msg):
+        self.argument_a = msg.argument_a
+        self.argument_b = msg.argument_b
+        self.get_logger().info('Timestamp of the message: {0}'.format(msg.stamp))
+        self.get_logger().info('Subscribed argument a: {0}'.format(self.argument_a))
+        self.get_logger().info('Subscribed argument b: {0}'.format(self.argument_b))
+
+    def get_arithmetic_operator(self, request, response):
+        self.argument_operator = request.arithmetic_operator
+        self.argument_result = self.calculate_given_formula(
+            self.argument_a,
+            self.argument_b,
+            self.argument_operator)
+        response.arithmetic_result = self.argument_result
+        self.argument_formula = '{0} {1} {2} = {3}'.format(
+                self.argument_a,
+                self.operator[self.argument_operator-1],
+                self.argument_b,
+                self.argument_result)
+        self.get_logger().info(self.argument_formula)
+        return response
+
+    def calculate_given_formula(self, a, b, operator):
+        if operator == ArithmeticOperator.Request.PLUS:
+            self.argument_result = a + b
+        elif operator == ArithmeticOperator.Request.MINUS:
+            self.argument_result = a - b
+        elif operator == ArithmeticOperator.Request.MULTIPLY:
+            self.argument_result = a * b
+        elif operator == ArithmeticOperator.Request.DIVISION:
+            try:
+                self.argument_result = a / b
+            except ZeroDivisionError:
+                self.get_logger().error('ZeroDivisionError!')
+                self.argument_result = 0.0
+                return self.argument_result
+        else:
+            self.get_logger().error(
+                'Please make sure arithmetic operator(plus, minus, multiply, division).')
+            self.argument_result = 0.0
+        return self.argument_result
+
+    def execute_checker(self, goal_handle):
+        self.get_logger().info('Execute arithmetic_checker action!')
+        feedback_msg = ArithmeticChecker.Feedback()
+        feedback_msg.formula = []
+        total_sum = 0.0
+        goal_sum = goal_handle.request.goal_sum
+        while total_sum < goal_sum:
+            total_sum += self.argument_result
+            feedback_msg.formula.append(self.argument_formula)
+            self.get_logger().info('Feedback: {0}'.format(feedback_msg.formula))
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(1)
+        goal_handle.succeed()
+        result = ArithmeticChecker.Result()
+        result.all_formula = feedback_msg.formula
+        result.total_sum = total_sum
+        return result
+    
+def main(args=None):
+    rclpy.init(args=args)
+    try:
+        calculator = Calculator()
+        executor = MultiThreadedExecutor(num_threads=4)
+        executor.add_node(calculator)
+        try:
+            executor.spin()
+        except KeyboardInterrupt:
+            calculator.get_logger().info('Keyboard Interrupt (SIGINT)')
+        finally:
+            executor.shutdown()
+            calculator.arithmetic_action_server.destroy()
+            calculator.destroy_node()
+    finally:
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
