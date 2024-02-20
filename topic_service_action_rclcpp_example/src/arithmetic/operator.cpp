4e2eda93fef0595d891fb6f7bedaa7aa12144270
Yeongmin Kim <88310751+yeongmin01@users.noreply.github.com>
2024-02-20 18:31:07 +0900

    service node client main() function

--- topic_service_action_rclcpp_example/src/arithmetic/operator.cpp
+++ topic_service_action_rclcpp_example/src/arithmetic/operator.cpp
@@ -51,3 +51,88 @@ void Operator::send_request()
   auto future_result =
     arithmetic_service_client_->async_send_request(request, response_received_callback);
 }
+
+int getch()
+{
+  struct termios oldt, newt;
+  int ch;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return ch;
+}
+
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if (ch != EOF) {
+    ungetc(ch, stdin);
+    return 1;
+  }
+  return 0;
+}
+
+bool pull_trigger()
+{
+  const uint8_t KEY_ENTER = 10;
+  while (1) {
+    if (kbhit()) {
+      uint8_t c = getch();
+      if (c == KEY_ENTER) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  return false;
+}
+
+void print_help()
+{
+  printf("For operator node:\n");
+  printf("node_name [-h]\n");
+  printf("Options:\n");
+  printf("\t-h Help           : Print this help function.\n");
+}
+
+int main(int argc, char * argv[])
+{
+  if (rcutils_cli_option_exist(argv, argv + argc, "-h")) {
+    print_help();
+    return 0;
+  }
+
+  rclcpp::init(argc, argv);
+
+  auto operator_node = std::make_shared<Operator>();
+
+  while (rclcpp::ok()) {
+    rclcpp::spin_some(operator_node);
+    operator_node->send_request();
+
+    printf("Press Enter for next service call.\n");
+    if (pull_trigger() == false) {
+      rclcpp::shutdown();
+      return 0;
+    }
+  }
+}
